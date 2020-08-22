d7fbd758496f96f45dfe743ddbb37e82101d7438
UberStash <expiredserver@gmail.com>
2020-08-22 22:55:22 +0000

    completed create-bookie button and jquery funcatnality'

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,68 @@
+$(document).ready(() => {
+  $('#form-content').hide();
+  $('#create-bookie').on('click', landingToForm);
+});
+function landingToForm() {
+
+    $('#render-form-page-container').hide();
+    $('#form-content').show();
+
+  }
+
+
+
+// $(document).ready(function() {
+
+
+
+
+
+
+
+
+
+//   $('#create-bookie').on('click', '#render-form-page-container', function(event) {
+
+//       $('#render-form-page-container').hide(0);
+
+//   });
+
+
+    // $.ajax({ url: '/render-form/', method: 'GET', dataType: 'JSON' })
+    //   .then(function(response) {
+    //     $('#render-form-page-container').slideup();
+
+    //       // $('#tweet-container').prepend(createTweetElement(obj));
+
+    //   });
+
+
+
+//   router.post('/properties', (req, res) => {
+//     //const userId = req.session.userId;
+//     database.addProperty({...req.body})
+//       .then(property => {
+//         res.send(property);
+//       })
+//       .catch(e => {
+//         console.error(e);
+//         res.send(e)
+//       });
+//   });
+
+//   return router;
+//
+
+
+
+
+
+
+
+
+
+
+// added in starter files
 $(() => {
   $.ajax({
     method: "GET",
