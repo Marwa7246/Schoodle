f33b4517c8ebde8ae4c44a9c94dfed05ad7f3c20
Marwa Ragheb <marwa.ragheb@gmail.com>
2020-08-23 22:40:29 +0000

    Add another 2 sql queries

---
+++ db/queries/3-voters_details_for_specific_poll_searching_by_url.sql
@@ -0,0 +1,6 @@
+SELECT time_slot_id, name, email, choice
+FROM votes
+JOIN time_slots ON time_slot_id=time_slots.id
+JOIN polls ON poll_id=polls.id
+WHERE polls.url='http://localhost:8080/?123'
+ORDER BY time_slot_id;
