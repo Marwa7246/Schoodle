efbc488bfb4a46f49e942db7b010c50c3b67cd99
Marwa Ragheb <marwa.ragheb@gmail.com>
2020-08-24 23:18:34 +0000

    working in get route

--- routes/polls.js
+++ routes/polls.js
@@ -24,14 +24,69 @@ module.exports = (db) => {
       });
   })
 
+
+  const getPollById = function(id) {
+    return db.query(`
+    SELECT title, id, location FROM polls
+    WHERE id=$1
+    `, [id])
+    .then(res => {
+      console.log('responseId: ', res.rows[0]);
+      return res.rows[0]});
+  }
+
+  //Get the information of a poll after using the url
+  // router.get("/:id", (req, res) => {
+  //   let id = req.params.id;
+  //   //let templateVars = getPollById(id);
+  //   getPollById(id).
+  //   then(data => {
+  //     console.log('responseId: ', data);
+  //     return data;
+  //   });
+
+  //   res.render('index', data);
+
+  // });
+
   router.get("/:id", (req, res) => {
-    let urlString = req.params.id;
+    let query = `SELECT * FROM polls`;
+    console.log(query);
+    db.query(query)
+      .then(data => {
+        const polls = data.rows;
 
-    let templateVars = { urlString }
-    console.log('templateVars: ' , templateVars);
-    res.render('index', templateVars)
-
+        const templateVars=JSON.parse(polls);
+        console.log(templateVars)
+        //res.json({ polls });
+        res.render('index', templateVars)
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
   });
+
+
+
+
+  // router.get("/:poll_id", (req, res) => {
+
+  //   let query = `SELECT * FROM polls WHERE id = $1`;
+  //   console.log(query);
+  //   db.query(query, [req.params.poll_id])
+  //     .then(data => {
+  //       res.json('ok');
+  //     })
+  //     .catch(err => {
+  //       res
+  //         .status(500)
+  //         .json({ error: err.message });
+  //     });
+  // })
+
+
 
   return router;
 };
